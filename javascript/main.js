2f941fca6f71d135ba7699bc6e37a22b64e0c467
David Fonseca <davidfonseca@davids-mbp.lan>
2021-10-13 20:57:07 +0300

    Added translate animations to home page.

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,9 @@ const toggleMenu = document.querySelector(".menu-toggle");
 const openMenu = document.querySelector(".menu-open");
 const closeMenu = document.querySelector(".menu-close");
 const navMenu = document.querySelector(".nav-menu");
+const mainTitle = document.querySelector(".main-title");
+const sideTitle = document.querySelector(".side-title");
+const cardsContainer = document.querySelector(".cards-container");
 
 //EVENT LISTENERS
 toggleMenu.addEventListener("click", () => {
@@ -11,4 +14,19 @@ toggleMenu.addEventListener("click", () => {
     openMenu.classList.toggle("hidden-icon");
     closeMenu.classList.toggle("hidden-icon");
   }, 150);
+});
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    sideTitle.style.transform = "translateX(0%)";
+  }, 2000);
+
+  setTimeout(() => {
+    mainTitle.style.transform = "translateX(0)";
+  }, 500);
+});
+
+window.addEventListener("scroll", () => {
+  cardsContainer.style.visibility = "visible";
+  cardsContainer.style.transform = "translateY(0%)";
 });
