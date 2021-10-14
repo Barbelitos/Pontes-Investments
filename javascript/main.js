7809184212c4047cd567783a365dcfc2cfab80b7
David Fonseca <davidfonseca@davids-mbp.lan>
2021-10-14 11:48:34 +0300

    Small changes to services page.

--- javascript/main.js
+++ javascript/main.js
@@ -6,6 +6,9 @@ const navMenu = document.querySelector(".nav-menu");
 const mainTitle = document.querySelector(".main-title");
 const sideTitle = document.querySelector(".side-title");
 const cardsContainer = document.querySelector(".cards-container");
+const wealthText = document.querySelector(".wealth-text");
+const investmentsText = document.querySelector(".investments-text");
+const advisorText = document.querySelector(".advisor-text");
 
 //EVENT LISTENERS
 toggleMenu.addEventListener("click", () => {
@@ -24,6 +27,10 @@ window.addEventListener("load", () => {
   setTimeout(() => {
     mainTitle.style.transform = "translateX(0)";
   }, 800);
+
+  wealthText.style.transform = "translateX(0)";
+  investmentsText.style.transform = "translateX(0)";
+  advisorText.style.transform = "translateX(0)";
 });
 
 window.addEventListener("scroll", () => {
